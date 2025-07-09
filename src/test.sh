58e4fe6517fe7ee8ceb87e820b949677688d5277
Mohamed Ibranim <mosuliman92@gmail.com>
2025-07-09 16:24:23 +0300

    create new node js application

---
+++ src/test.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+EXPECTED="Hellow, Test"
+
+OUTPUT=$(node -e "console.log(require('./src/app.js')('Test'))")
+
+if ["$EXPECTED" == "$OUTPUT"]; then
+
+echo " Test passed!"
+
+exit 0
+
+else
+
+echo "Test failed, Expected '$EXPECTED' but got '$OUTPUT'"
+
+exit 1
+
+fi
